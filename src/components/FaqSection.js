632b68078b4875043fe42d98e234290c05a56c95
Acer <shaxzodbekbobobekov@gmail.com>
2022-12-07 15:35:30 +0500

    FAQ section added

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function FaqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>{" "}
+      </h2>
+      <div className="question">
+        <h4>Can i laerning Lorem ipsum dolor sit amet.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt,
+            eius?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Can i laerning Lorem ipsum dolor sit amet.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt,
+            eius?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Can i laerning Lorem ipsum dolor sit amet.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt,
+            eius?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Can i laerning Lorem ipsum dolor sit amet.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt,
+            eius?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import BenefitsSection from "../components/BenefitsSection";
+import FaqSection from "../components/FaqSection";
 
 function AboutUs() {
   return (
     <div>
       <AboutSection />
       <BenefitsSection />
+      <FaqSection />
     </div>
   );
 }
