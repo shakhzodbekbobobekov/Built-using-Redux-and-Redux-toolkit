9b161bc399b211e5d04a6e4a4f0db73d77c32a0c
Acer <shaxzodbekbobobekov@gmail.com>
2022-12-12 15:35:44 +0500

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import GlobalStyle from "./components/GlobalStyle";
 
 import AboutUs from "./pages/AboutUs";
-
+// console.log("asfgd")
 function App() {
   return (
     <div className="App">
